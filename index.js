25f479488e83284730fa771597284fe3330ef5d1
WillRobinson152 <robinsonwilliamarthur@gmail.com>
2022-04-11 10:23:18 -0500

    Added routing. Installed react.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function Spa(){
     return (
-        <>
-        <h1>Welcome to Bad Bank</h1>
-        <NavBar/>
-        </>
+        <HashRouter>
+            <NavBar/>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" exact component={CreateAccount} />
+            <Route path="/login/" exact component={Login} />
+            <Route path="/deposit/" exact component={Deposit} />
+            <Route path="/withdraw/" exact component={Withdraw} />
+            <Route path="/balance/" exact component={Balance} />
+            <Route path="/alldata/" exact component={AllData} />
+        </HashRouter>
     );
 }
 
